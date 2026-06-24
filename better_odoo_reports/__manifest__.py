b7153a28c76dc34b9b5073a504b8819c15b31576
Mansoor Sohail 2424 <92199656+syphen10@users.noreply.github.com>
2026-06-24 18:53:15 +0500

    Update __manifest__.py

--- better_odoo_reports/__manifest__.py
+++ better_odoo_reports/__manifest__.py
@@ -2,9 +2,14 @@
     'name': 'Better Reports - Prism Engine',
     'version': '1.0',
     'category': 'Reporting',
-    'summary': 'Generates high-performance interactive SPA reports',
-    'depends': ['base', 'web'],
+    'summary': 'Generates high-performance interactive SPA financial reports',
+    'depends': [
+        'base', 
+        'web', 
+        'account'  # Required because we are now querying the financial ledgers
+    ],
     'data': [
+        'security/security.xml',
         'security/ir.model.access.csv',
         'views/better_report_views.xml',
         'views/better_report_wizard_views.xml',
